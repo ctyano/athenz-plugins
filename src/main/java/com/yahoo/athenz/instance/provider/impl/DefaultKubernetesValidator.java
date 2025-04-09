831353597f73884011bd9310ba9ba52b4ccff897
Tatsuya Yano <tatyano@yahoo-corp.jp>
2025-04-09 10:52:20 +0900

    Fixed InstanceK8sProvider related implementation

--- src/main/java/com/yahoo/athenz/instance/provider/impl/DefaultKubernetesValidator.java
+++ src/main/java/com/yahoo/athenz/instance/provider/impl/DefaultKubernetesValidator.java
@@ -3,11 +3,7 @@ package com.yahoo.athenz.instance.provider.impl;
 import com.yahoo.athenz.auth.Authorizer;
 import com.yahoo.athenz.auth.Principal;
 import com.yahoo.athenz.auth.impl.SimplePrincipal;
-import com.yahoo.athenz.common.server.util.config.dynamic.DynamicConfigCsv;
-import com.yahoo.athenz.instance.provider.AttrValidator;
-import com.yahoo.athenz.instance.provider.AttrValidatorFactory;
 import com.yahoo.athenz.instance.provider.InstanceConfirmation;
-import com.yahoo.athenz.instance.provider.InstanceProvider;
 import javax.net.ssl.SSLContext;
 import org.eclipse.jetty.util.StringUtil;
 import org.slf4j.Logger;
@@ -16,93 +12,59 @@ import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 import java.util.*;
 
-import static com.yahoo.athenz.common.server.util.config.ConfigManagerSingleton.CONFIG_MANAGER;
 import static com.yahoo.athenz.instance.provider.impl.InstanceGCPProvider.*;
 
-public class DefaultGCPGoogleKubernetesEngineValidator extends CommonKubernetesDistributionValidator {
+public class DefaultKubernetesValidator extends CommonKubernetesDistributionValidator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    Set<String> gcpDNSSuffixes = new HashSet<>();
-    List<String> gkeDnsSuffixes;
-    DynamicConfigCsv gkeClusterNames;
+    Set<String> k8sDNSSuffixes;
 
-    private static final DefaultGCPGoogleKubernetesEngineValidator INSTANCE = new DefaultGCPGoogleKubernetesEngineValidator();
-    static final String GCP_OIDC_ISSUER_PREFIX = "https://container.googleapis.com/v1/projects/";
-    AttrValidator attrValidator;
-    static final String ZTS_PROP_K8S_PROVIDER_GCP_ATTR_VALIDATOR_FACTORY_CLASS = "athenz.zts.k8s_provider_gcp_attr_validator_factory_class";
+    private static final DefaultKubernetesValidator INSTANCE = new DefaultKubernetesValidator();
+    static final String K8S_OIDC_ISSUER                   = "https://kubernetes.default.svc.cluster.local";
+    static final String K8S_PROP_BOOT_TIME_OFFSET         = "athenz.zts.k8s_boot_time_offset";
+    static final String K8S_PROP_DNS_SUFFIX               = "athenz.zts.k8s_dns_suffix";
 
-    public static DefaultGCPGoogleKubernetesEngineValidator getInstance() {
+    public static DefaultKubernetesValidator getInstance() {
         return INSTANCE;
     }
-    private DefaultGCPGoogleKubernetesEngineValidator() {
-    }
-
-    static AttrValidator newAttrValidator(final SSLContext sslContext) {
-        final String factoryClass = System.getProperty(ZTS_PROP_K8S_PROVIDER_GCP_ATTR_VALIDATOR_FACTORY_CLASS);
-        LOGGER.info("GCP K8S AttributeValidatorFactory class: {}", factoryClass);
-        if (factoryClass == null) {
-            return null;
-        }
-
-        AttrValidatorFactory attrValidatorFactory;
-        try {
-            attrValidatorFactory = (AttrValidatorFactory) Class.forName(factoryClass).getConstructor().newInstance();
-        } catch (Exception e) {
-            LOGGER.error("Invalid AttributeValidatorFactory class: {}", factoryClass, e);
-            throw new IllegalArgumentException("Invalid AttributeValidatorFactory class");
-        }
-
-        return attrValidatorFactory.create(sslContext);
+    private DefaultKubernetesValidator() {
     }
 
     @Override
     public void initialize(final SSLContext sslContext, Authorizer authorizer) {
         super.initialize(sslContext, authorizer);
-        final String dnsSuffix = System.getProperty(GCP_PROP_DNS_SUFFIX);
+        final String dnsSuffix = System.getProperty(K8S_PROP_DNS_SUFFIX);
         if (!StringUtil.isEmpty(dnsSuffix)) {
-            gcpDNSSuffixes.addAll(Arrays.asList(dnsSuffix.split(",")));
+            k8sDNSSuffixes.addAll(Arrays.asList(dnsSuffix.split(",")));
         }
-        // get our allowed gke dns suffixes
-        gkeDnsSuffixes = InstanceUtils.processK8SDnsSuffixList(GCP_PROP_GKE_DNS_SUFFIX);
-        // get our dynamic list of gke cluster names
-        gkeClusterNames = new DynamicConfigCsv(CONFIG_MANAGER, GCP_PROP_GKE_CLUSTER_NAMES, null);
-        this.attrValidator = newAttrValidator(sslContext);
     }
 
     @Override
     public String validateIssuer(InstanceConfirmation confirmation, IdTokenAttestationData attestationData, StringBuilder errMsg) {
         String issuer = getIssuerFromToken(attestationData, errMsg);
         if (StringUtil.isEmpty(issuer)) {
+            errMsg.append("Issuer is empty");
             return null;
         }
-        String gcpProject = confirmation.getAttributes().get(InstanceProvider.ZTS_INSTANCE_GCP_PROJECT);
-        if (!issuer.startsWith(GCP_OIDC_ISSUER_PREFIX + gcpProject)) {
-            // could be a multi-tenant setup where the issuer is not present in the identity's GCP project
-            if (attrValidator != null) {
-                confirmation.getAttributes().put(ZTS_INSTANCE_UNATTESTED_ISSUER, issuer);
-                // Confirm the issuer as per the attribute validator
-                if (!attrValidator.confirm(confirmation)) {
-                    return null;
-                }
-            } else {
-                errMsg.append("Issuer is not present in the GCP project associated with the domain");
-                return null;
-            }
-        } else {
-            // issuer exists in the same GCP project as the requested identity
-            confirmation.getAttributes().put(ZTS_INSTANCE_ISSUER_GCP_PROJECT, gcpProject);
+        if (!issuer.equals(K8S_OIDC_ISSUER)) {
+            errMsg.append("Issuer is not ").append(K8S_OIDC_ISSUER);
+            return null;
         }
+        final Map<String, String> instanceAttributes = confirmation.getAttributes();
+        final String cloudName = InstanceUtils.getInstanceProperty(instanceAttributes, ZTS_INSTANCE_CLOUD);
+        final String providerName = confirmation.getProvider();
+        final String k8sNamespaceName = attestationData.getIdentityToken();
+        final String k8sServiceAccountName = attestationData.getIdentityToken();
 
         final String domainName = confirmation.getDomain();
         final String serviceName = confirmation.getService();
         // attribute set after verification above or attribute validation
-        final String issuerGcpProject = confirmation.getAttributes().get(ZTS_INSTANCE_ISSUER_GCP_PROJECT);
-        final String resource = String.format("%s:%s:%s", domainName, serviceName, issuerGcpProject);
+        final String resource = String.format("cloud:%s:provider:%s:system:serviceaccount:%s:%s", cloudName, providerName, k8sNamespaceName, k8sServiceAccountName);
 
         Principal principal = SimplePrincipal.create(domainName, serviceName, (String) null);
         boolean accessCheck = authorizer.access(ACTION_LAUNCH, resource, principal, null);
         if (!accessCheck) {
-            errMsg.append("gke launch authorization check failed for action: ").append(ACTION_LAUNCH)
+            errMsg.append("k8s launch authorization check failed for action: ").append(ACTION_LAUNCH)
                     .append(" resource: ").append(resource);
             return null;
         }
@@ -113,15 +75,14 @@ public class DefaultGCPGoogleKubernetesEngineValidator extends CommonKubernetesD
     public boolean validateSanDNSEntries(InstanceConfirmation confirmation, StringBuilder errMsg) {
         StringBuilder instanceId = new StringBuilder(256);
         final Map<String, String> instanceAttributes = confirmation.getAttributes();
-        final String gcpProject = InstanceUtils.getInstanceProperty(instanceAttributes, ZTS_INSTANCE_GCP_PROJECT);
-        if (StringUtil.isEmpty(gcpProject)) {
-            errMsg.append("Unable to find GCP project id");
+        final String cloudName = InstanceUtils.getInstanceProperty(instanceAttributes, ZTS_INSTANCE_CLOUD);
+        if (StringUtil.isEmpty(cloudName) || cloudName.equals("")) {
+            errMsg.append("Unable to find cloud name");
             return false;
         }
         if (!InstanceUtils.validateCertRequestSanDnsNames(instanceAttributes, confirmation.getDomain(),
-                confirmation.getService(), gcpDNSSuffixes, gkeDnsSuffixes, gkeClusterNames.getStringsList(),
-                true, instanceId, null)) {
-            errMsg.append("Unable to validate certificate request hostnames");
+                confirmation.getService(), k8sDNSSuffixes, null, null, false, instanceId, null)) {
+            errMsg.append("Unable to validate certificate request hostnames for SAN DNS names");
             return false;
         }
         return true;
--- src/main/java/com/yahoo/athenz/instance/provider/impl/SimpleKubernetesDistributionValidatorFactory.java
+++ src/main/java/com/yahoo/athenz/instance/provider/impl/SimpleKubernetesDistributionValidatorFactory.java
@@ -6,15 +6,14 @@ import com.yahoo.athenz.instance.provider.KubernetesDistributionValidatorFactory
 import java.util.HashMap;
 import java.util.Map;
 
-public class DefaultKubernetesDistributionValidatorFactory implements KubernetesDistributionValidatorFactory {
+public class SimpleKubernetesDistributionValidatorFactory implements KubernetesDistributionValidatorFactory {
 
     Map<String, KubernetesDistributionValidator> supportedDistributionsMap = new HashMap<>();
-    static final String CLOUD_AWS = "aws";
-    static final String CLOUD_GCP = "gcp";
+    static final String CLOUD_K8S = "k8s";
+    
     @Override
     public void initialize() {
-        supportedDistributionsMap.put(CLOUD_AWS, DefaultAWSElasticKubernetesServiceValidator.getInstance());
-        supportedDistributionsMap.put(CLOUD_GCP, DefaultGCPGoogleKubernetesEngineValidator.getInstance());
+        supportedDistributionsMap.put(CLOUD_K8S, DefaultKubernetesValidator.getInstance());
     }
 
     @Override
