fd1ec592977249e7d588a13116cb8ee973bbb8c7
ctyano <ctyano@duck.com>
2026-06-12 21:58:03 +0900

    Added EmailTokenExchangeIdentityProvider

---
+++ src/main/java/com/yahoo/athenz/auth/impl/EmailTokenExchangeIdentityProvider.java
@@ -0,0 +1,32 @@
+package com.yahoo.athenz.auth.impl;
+
+import com.yahoo.athenz.auth.TokenExchangeIdentityProvider;
+import com.yahoo.athenz.auth.token.OAuth2Token;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class EmailTokenExchangeIdentityProvider implements TokenExchangeIdentityProvider {
+
+    @Override
+    public String getTokenIdentity(final OAuth2Token token) {
+        final Object emailClaim = token.getClaim("email");
+        if (emailClaim == null) {
+            return null;
+        }
+
+        final String email = emailClaim.toString().trim().toLowerCase(Locale.ROOT);
+        return email.isEmpty() ? null : "email:ext." + email;
+    }
+
+    @Override
+    public String getTokenAudience(final OAuth2Token token) {
+        return token.getAudience();
+    }
+
+    @Override
+    public List<String> getTokenExchangeClaims() {
+        return Collections.singletonList("email");
+    }
+}
